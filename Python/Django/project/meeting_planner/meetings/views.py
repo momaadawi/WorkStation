f4ee1778239e9e1710c7e614a062da8b009be403
[momaadawi] <[mo.maadawi@gmail.com]>
2021-07-08 04:41:59 +0200

    form meetings

--- Python/Django/project/meeting_planner/meetings/views.py
+++ Python/Django/project/meeting_planner/meetings/views.py
@@ -1,6 +1,6 @@
 from meetings.models import Meeting, Room
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import MeetingForm
 # Create your views here.
 def detail(request, id: int):
     meeting = get_object_or_404(Meeting, pk=id)    
@@ -9,3 +9,14 @@ def detail(request, id: int):
 
 def room_list(request):
     return render(request, "meetings/rooms_list.html", { "rooms" :  Room.objects.all()})
+
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('welcome')
+    else:            
+        form = MeetingForm()
+        return render(request, "meetings/new.html", {"from": form})
